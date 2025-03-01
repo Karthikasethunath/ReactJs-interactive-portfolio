dfb8be13f8e416f3b75ed4c86aa33e14ac328b54
Karthika Sethunath <keerthisnath123@gmail.com>
2025-03-01 14:28:17 +1100

    changed images

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -1,6 +1,9 @@
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/chat-bot.png';
 import freshBurger from '../../assets/new-normality.png';
+import coding from '../../assets/coding.png';
+import machinelearning from '../../assets/machine-learning.png';
+import upload from '../../assets/upload.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -15,11 +18,29 @@ function Projects() {
             p="Derma-AI chatbot"
         />
         <ProjectCard 
+            src={coding} 
+            link="https://github.com/Karthikasethunath/AI-job-displacement"
+            h3="React js project"
+            p="Portfolio website creation"
+        />
+        <ProjectCard 
             src={freshBurger} 
             link="https://github.com/Karthikasethunath/AI-job-displacement"
             h3="Power BI project"
             p="Covid-19 Dashboard"
         />
+        <ProjectCard 
+            src={upload} 
+            link="https://github.com/Karthikasethunath/AI-job-displacement"
+            h3="AWS project"
+            p="Cloud computing"
+        />
+        <ProjectCard 
+            src={machinelearning} 
+            link="https://github.com/Karthikasethunath/AI-job-displacement"
+            h3="Explainable ML project"
+            p="GradCAM Variations"
+        />    
       </div>
     </section>
   );
